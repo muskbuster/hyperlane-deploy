797b8db9475c4107f5d7c16784e5d772c57c470f
Yash Garg <yashygkf@gmail.com>
2023-08-14 10:11:26 +0530

    feat: fantom testnet wrap token

--- artifacts/warp-ui-token-list.ts
+++ artifacts/warp-ui-token-list.ts
@@ -6,5 +6,6 @@ export const tokenList = [
 {"chainId":84531,"name":"ETH","symbol":"ETH","decimals":18,"type":"native","hypNativeAddress":"0xa61B779A062F31Be462E8E2F3bDD311308EBb2A6"},
 {"chainId":59140,"name":"ETH","symbol":"ETH","decimals":18,"type":"native","hypNativeAddress":"0xE2C984a4faD8E9232D1bb0c2b9978E854cafcf21"},
 {"chainId":5001,"name":"MNT","symbol":"MNT","decimals":18,"type":"native","hypNativeAddress":"0x782D510bDa59111907434aC9d73FE9fAE1C0774d"},
-{"chainId":8081,"name":"shardeum","symbol":"SHM","decimals":18,"type":"native","hypNativeAddress":"0x158fe303274322204E5233c971CB676d768Db782"}
+{"chainId":8081,"name":"shardeum","symbol":"SHM","decimals":18,"type":"native","hypNativeAddress":"0x158fe303274322204E5233c971CB676d768Db782"},
+{"chainId":4002,"name":"FTM","symbol":"FTM","decimals":18,"type":"native","hypNativeAddress":"0x1b4c2C130E3e96e9e39416Aaf63F1428A2e2B0c2"}
 ];
